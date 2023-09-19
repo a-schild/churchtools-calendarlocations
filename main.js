b1e631b9595f37b80e9f37d8a2d9479fab2bde63
André Schild <a.schild@aarboard.ch>
2023-09-19 17:55:33 +0200

    Retrieve all calendars the user has access to

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const { ChurchToolsClient, activateLogging } = require('@churchtools/churchtools-client');
 const axiosCookieJarSupport = require('axios-cookiejar-support');
 const tough = require('tough-cookie');
-const fs = require('fs');
 
-//
-// Load config from config.json
-//
-let rawdata = fs.readFileSync('config.json');
-let config = JSON.parse(rawdata);
-
-console.warn(config.site_url);
-console.warn(config.auth_token);
+const myCT= new ChurchToolsClient();
+myCT.setCookieJar(axiosCookieJarSupport.wrapper, new tough.CookieJar());
+myCT.setBaseUrl("https://tools.ref-nidau.ch");
+myCT.setUnauthorizedInterceptor("LniCnweyFaCv3KSq02MbDeRVzQsU27W8VvivhIgtN0m8ZwPImU0GqxnumSVyznyRGLpI8td73PN7E8u8RBLT1irlRLjtJNOZiVTiMAN0jA02ynhIzL83t95BVnrL1DHBpwGIt2DK9B9jYZCcG3mJfnMenqWYQnToJMjJXNYjqffFVZ0mrgJbFWutXvnQUfXyJB6674cNVQVyq4zRGD73NyCjtidgxDXcsRJTb0fenR3SEed46zCmuBhmR3OwfcBI");
 activateLogging();
 
-const myCT= new ChurchToolsClient(config.site_url, config.auth_token);
+//myCT.get('/whoami?only_allow_authenticated=true').then(whoAmI => {
+//    console.dir(whoAmI);
+//    console.log(`Hello ${whoAmI.firstName} ${whoAmI.lastName}!`);
+//});
 
-myCT.get('/whoami').then(whoAmI => {
-    console.dir(whoAmI);
-    console.log(`Hello ${whoAmI.firstName} ${whoAmI.lastName}!`);
+myCT.get('/calendars').then(all_calendars => {
+    for (var cal in all_calendars) {
+        if (all_calendars.hasOwnProperty(cal)) {
+            console.log('Calendar: '+all_calendars[cal].id+' name: '+all_calendars[cal].name+' private: '+all_calendars[cal].isPrivate+" public "+all_calendars[cal].isPublic);
+        }
+    }
 });
