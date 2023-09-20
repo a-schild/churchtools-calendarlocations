fc71f5b765baa54f2ff8721ed4b67b7eeff84fa8
André Schild <a.schild@aarboard.ch>
2023-09-20 11:38:37 +0200

    Added logging framework, first loops to check calendar processing

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const { ChurchToolsClient, activateLogging } = require('@churchtools/churchtools-client');
 const axiosCookieJarSupport = require('axios-cookiejar-support');
 const tough = require('tough-cookie');
+const fs = require('fs');
+const pino = require('pino');
+
+// Init logger
+const logger = pino({
+  level: 'info'
+//  level: 'debug'
+});
+
+//
+// Load config from config.json
+//
+let rawdata = fs.readFileSync('config.json');
+let config = JSON.parse(rawdata);
 
 const myCT= new ChurchToolsClient();
 myCT.setCookieJar(axiosCookieJarSupport.wrapper, new tough.CookieJar());
-myCT.setBaseUrl("https://tools.ref-nidau.ch");
-myCT.setUnauthorizedInterceptor("LniCnweyFaCv3KSq02MbDeRVzQsU27W8VvivhIgtN0m8ZwPImU0GqxnumSVyznyRGLpI8td73PN7E8u8RBLT1irlRLjtJNOZiVTiMAN0jA02ynhIzL83t95BVnrL1DHBpwGIt2DK9B9jYZCcG3mJfnMenqWYQnToJMjJXNYjqffFVZ0mrgJbFWutXvnQUfXyJB6674cNVQVyq4zRGD73NyCjtidgxDXcsRJTb0fenR3SEed46zCmuBhmR3OwfcBI");
+myCT.setBaseUrl(config.site_url);
+myCT.setUnauthorizedInterceptor(config.auth_token);
 activateLogging();
 
 //myCT.get('/whoami?only_allow_authenticated=true').then(whoAmI => {
@@ -14,9 +28,24 @@ activateLogging();
 //});
 
 myCT.get('/calendars').then(all_calendars => {
+    // Loop through all calendars visible for the user
     for (var cal in all_calendars) {
         if (all_calendars.hasOwnProperty(cal)) {
-            console.log('Calendar: '+all_calendars[cal].id+' name: '+all_calendars[cal].name+' private: '+all_calendars[cal].isPrivate+" public "+all_calendars[cal].isPublic);
+            let calFound= false;
+            // Check to see if it's one the configured calender from config.json
+            for (let calConfig in config.process_calendars) {
+                if (config.process_calendars[calConfig].id === all_calendars[cal].id) {
+                    //logger.info("Found in config: "+config.process_calendars[calConfig].id);
+                    calFound= true;
+                } else {
+                    //logger.debug("Not found in config: "+config.process_calendars[calConfig].id+" "+all_calendars[cal].id);
+                }
+            }
+            if (calFound) {
+                logger.info('Processing calendar: '+all_calendars[cal].id+' name: '+all_calendars[cal].name+' private: '+all_calendars[cal].isPrivate+" public "+all_calendars[cal].isPublic);
+            } else {
+                logger.debug("Ignoring calender with ID "+cal+" ("+all_calendars[cal].name+")");
+            }
         }
     }
 });
